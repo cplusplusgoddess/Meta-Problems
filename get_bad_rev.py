434f3a9c3ef6c6baae5fd61fbb2bdda944866437
cplusplusgoddess <amberwaves@nethere.com>
2022-10-21 00:08:59 -0700

    Add files via upload
    starts from the known bad and traverses thru the parent to find the first good commit rev

--- get_bad_rev.py
+++ get_bad_rev.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Your team is working on a git repository, with everyone committing code.
-
 # Author: Amber Rogowicz
-# At a certain point, a bug is introduced into the repo by a revision. 
+# Your team is working on a git repository, with everyone committing code.
+# At a certain point, a bug is introduced into the repo by a revision.
 # All subsequent revisions will have the same bug,
 # while all revisions before that do not.
-
+#
 # Your task is, given a known good revision and a bad revision,
 # find the revision that introduced the bug.
-
-# Assumptions: 
-#  - You can assume a predefined function `build` which can build the 
+#
+# Assumptions:
+#  - You can assume a predefined function `build` which can build the
 # code of a given revision, and print the output. Dummy version here
-#  - The bug can be determined by a string keyword from the build output, 
+#  - The bug can be determined by a string keyword from the build output,
 # defined by the global variable `ERROR_STR`.
-
+#
 # Find the git revision that broke the build - i.e. outputs ERROR_STR
 # between git GOOD_REV and BAD_REV
 import argparse
@@ -27,7 +26,7 @@ ERROR_STR = "This is BAD"
 
 
 def build(git_rev) -> str:
-	return ERROR_STR + "more junk"
+    return ERROR_STR
 
 
 def repo_find(path=".", required=True):
@@ -46,31 +45,8 @@ def repo_find(path=".", required=True):
         else:
             return None
 
-    # cd .. 
+    # cd ..
     return repo_find(parent, required)
-
-
-def find_commits() -> list:
-    tree_root = repo_find()
-    if not tree_root:
-        return None
-
-    from subprocess import run, PIPE, STDOUT
-    result = run(["git", "rev-list", "--all"], shell=False,
-                 stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
-                 cwd=tree_root)
-
-    git_rev = result.stdout
-    git_rev = git_rev.decode()
-    git_rev = git_rev.rstrip()
-
-    assert result.returncode is not None
-    if result.returncode != 0:
-        from warnings import warn
-        warn("unable to find git revision")
-        return None
-    rev_list = git_rev.split()
-    return rev_list
 
 
 def find_git_revision(rev) -> dict:
@@ -80,9 +56,16 @@ def find_git_revision(rev) -> dict:
         return None
 
     from subprocess import run, PIPE, STDOUT
-    result = run(["git", "cat-file", "-p", rev], shell=False,
-                 stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
-                 cwd=tree_root)
+
+    result = run(
+        ["git", "cat-file", "-p", rev],
+        shell=False,
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=STDOUT,
+        close_fds=True,
+        cwd=tree_root,
+    )
 
     git_rev = result.stdout
     git_rev = git_rev.decode()
@@ -91,6 +74,7 @@ def find_git_revision(rev) -> dict:
     assert result.returncode is not None
     if result.returncode != 0:
         from warnings import warn
+
         warn("unable to find git revision")
         return None
     rev_dict = dict_from_result(git_rev)
@@ -114,23 +98,24 @@ def dict_from_result(result: str) -> dict:
     return ret_dict
 
 
-def find_first_bad_build(last_known_good) -> str:
+def find_first_good_build(first_known_bad) -> ():
     git_dict = {}
-    curr_commit = last_known_good
+    prev_commit = curr_commit = first_known_bad
     git_dict = find_git_revision(curr_commit)
     global ERROR_STR
     while git_dict is not None:
         if git_dict is None or "tree" not in git_dict:
-            return None
+            return None, None
         tree = git_dict["tree"][0]
-        if build(tree).find(ERROR_STR) >= 0:
-            return curr_commit
+        if build(tree).find(ERROR_STR) < 0:
+            return curr_commit, prev_commit
         if "parent" in git_dict:
+            prev_commit = curr_commit
             curr_commit = git_dict["parent"][0]
             git_dict = find_git_revision(curr_commit)
         else:
-            return None
-    return None
+            return None, None
+    return None, None
 
 
 def init_get_args(argv):
@@ -139,45 +124,39 @@ def init_get_args(argv):
     param: argv: str
     :return: good: str:bad: str
     """
-    argparser = argparse.ArgumentParser(prog="get_bad_rev",
-                                        description="FIRST Git revision FOOBAR FINDER")
-    argparser.required = True
-    argparser.add_argument(
-        "-g",
-        "--good",
-        metavar="Good Revision",
-        help="Git hash revision known to produce good build",
+    argparser = argparse.ArgumentParser(
+        prog="get_bad_rev", description="FIRST Git revision FOOBAR FINDER"
     )
 
     argparser.add_argument(
         "-b",
         "--bad",
-        metavar="Bad Revision",
+        metavar="Bad rev short or long hash",
         help="Git hash revision known to produce bad build",
+        required=True,
     )
-
     return argparser.parse_args(argv)
 
 
 def main(argv=sys.argv[1:]):
     """
-    command line arguments expected:
-     `-g GOOD_HASH_COMMIT
-     `-b BAD_HASH_COMMIT
-    param: argv: str
-    :return: good: str:bad: str
-    """
+	command line arguments expected:
+	`-b BAD_HASH_COMMIT
+	`-l List commit revs (optional)
+	param: argv: str
+	:return: good: str:bad: str
+	"""
     args = init_get_args(argv)
 
-    # Start at known good build commit and work up by parent
-    # check that rev exists and that it is a good build
-    # i.e. build does not return ERROR_STR
+    # Start at known bad build commit and work up by parent
+    # until a good found i.e. build does not return ERROR_STR
 
-    rev_id = find_first_bad_build(args.good)
-    if(rev_id):
-        print(f"First bad commit rev: {rev_id}\n")
-    else:
-        print(f"Could not find bad commit after rev: {args.good}\n")
+    if args.bad:
+        good_rev, bad_rev = find_first_good_build(args.bad)
+        if good_rev and bad_rev:
+            print(f"Last good rev: {good_rev} before first bad commit rev: {bad_rev}\n")
+        else:
+            print(f"Could not find good revision before rev: {args.bad}\n")
 
 
 if __name__ == "__main__":
